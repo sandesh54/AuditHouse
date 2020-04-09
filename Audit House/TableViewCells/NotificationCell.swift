28b68fc393ea5479cad5d0398325762d37f5669f
Sandesh Naik <sandeshnaik@doshaheen.com>
2020-04-09 17:00:14 +0530

    Added table view cell for notifications and their subviews

---
+++ Audit House/TableViewCells/NotificationCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationCell.swift
+//  Audit House
+//
+//  Created by Sandesh on 09/04/20.
+//  Copyright © 2020 Doshaheen Solutions Pvt Ltd. All rights reserved.
+//
+
+import UIKit
+
+class NotificationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
